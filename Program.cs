bb8a25df951ccda919427622f1376355da2afc05
Moyin Olusona <X00185164@mytudublin.ie>
2024-12-11 23:23:22 +0000

    Remove bin and obj directories from Git tracking

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using NobelPrizeApp.Services;
+using NobelPrizeApp; // Add this line to include the namespace for the App component
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
